f23cea2167b71645c35c639601627893b53dc817
matssondev <patric@matsson.dev>
2025-12-25 11:55:51 +0100

    Optimised file read and added config.py to handle configuration values

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,14 +1,19 @@
 import os
 
+from config import MAX_CHARS
 
+
+# Get content of a file within the permitted working directory
 def get_file_content(working_directory, file_path):
     if working_directory not in file_path:
-        raise ValueError(
-            f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        )
-    elif not os.path.isfile(file_path):
-        raise ValueError(f"File '{file_path}' does not exist")
-    else:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(file_path):
+        return f'Error: "{file_path}" is not a file or does not exist'
+
+    try:
         with open(file_path, "r") as file:
-            content = file.read()
+            content = file.read(MAX_CHARS)
         return content
+    except Exception as e:
+        return f"Error: {e}"
